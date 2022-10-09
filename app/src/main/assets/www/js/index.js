6379953bf52e828c652dacd8211b3032153d6650
DealUnloker <dealunloker@gmail.com>
2022-10-09 11:13:18 +0300

    done

--- app/src/main/assets/www/js/index.js
+++ app/src/main/assets/www/js/index.js
@@ -1,29 +1,115 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+document
+  .getElementById("accelerometer-start")
+  .addEventListener("click", startAcceleration);
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document
+  .getElementById("accelerometer-stop")
+  .addEventListener("click", stopAcceleration);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+document
+  .getElementById("capture-photo__btn")
+  .addEventListener("click", capturePhoto);
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+document
+  .getElementById("capture-photo-edit__btn")
+  .addEventListener("click", capturePhotoEdit);
+
+document
+  .getElementById("from-lib-photo__btn")
+  .addEventListener("click", getPhotoFromLib);
+
+document
+  .getElementById("from-album-photo__btn")
+  .addEventListener("click", getPhotoFromAlbum);
+
+let watchID = null;
+
+function startAcceleration() {
+  let options = { frequency: 1000 };
+
+  watchID = navigator.accelerometer.watchAcceleration(
+    onSuccess,
+    onError,
+    options
+  );
+}
+
+function stopAcceleration() {
+  if (watchID) {
+    navigator.accelerometer.clearWatch(watchID);
+    watchID = null;
+  }
+}
+
+function onSuccess(acceleration) {
+  let element = document.getElementById("accelerometer-text");
+
+  element.innerHTML =
+    "Acceleration X: " +
+    acceleration.x +
+    "<br />" +
+    "Acceleration Y: " +
+    acceleration.y +
+    "<br />" +
+    "Acceleration Z: " +
+    acceleration.z +
+    "<br />" +
+    "Timestamp: " +
+    acceleration.timestamp +
+    "<br />";
+}
+
+function onError() {
+  alert("onError!");
+}
+
+function capturePhoto() {
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+    quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL,
+  });
+}
+
+function capturePhotoEdit() {
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+    quality: 20,
+    allowEdit: true,
+    destinationType: Camera.DestinationType.DATA_URL,
+  });
+}
+
+function getPhotoFromLib() {
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+  });
+}
+
+function getPhotoFromAlbum() {
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
+  });
+}
+
+function onPhotoDataSuccess(imageData) {
+  let smallImage = document.getElementById("smallImage");
+
+  smallImage.style.display = "block";
+
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onPhotoURISuccess(imageURI) {
+  let largeImage = document.getElementById("largeImage");
+
+  largeImage.style.display = "block";
+
+  largeImage.src = imageURI;
+}
+
+function onFail(message) {
+  alert("Failed because: " + message);
 }
